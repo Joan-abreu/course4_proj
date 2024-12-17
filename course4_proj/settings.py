885474a8457dc6216765a2075140568e9d1b7d80
Joan Abreu <joan.abreu2007@gmail.com>
2024-12-17 14:38:06 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -49,14 +49,15 @@ class Dev(Configuration):
 				'django.contrib.messages',
 				'django.contrib.staticfiles',
 				'movies',
-				'gh'
+				'gh',
+				'django_celery_results'
 		]
 
 		MIDDLEWARE = [
 				'django.middleware.security.SecurityMiddleware',
 				'django.contrib.sessions.middleware.SessionMiddleware',
 				'django.middleware.common.CommonMiddleware',
-				# 'django.middleware.csrf.CsrfViewMiddleware',
+				'django.middleware.csrf.CsrfViewMiddleware',
 				'django.contrib.auth.middleware.AuthenticationMiddleware',
 				'django.contrib.messages.middleware.MessageMiddleware',
 				# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -158,3 +159,6 @@ class Dev(Configuration):
             "level": "DEBUG",
         },
     }
+
+		CELERY_RESULT_BACKEND = "django-db"
+		CELERY_BROKER_URL = "redis://localhost:6379/0"
